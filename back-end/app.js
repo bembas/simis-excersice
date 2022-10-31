4b6b9fdcdafd078668b8f558ee97d8b5c536c19c
errikos22 <esimintiridis@gmail.com>
2022-10-31 17:54:16 +0200

    First Commit of App

---
+++ back-end/app.js
@@ -0,0 +1,123 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static("../front-end"));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+const uri =
+  "mongodb+srv://errikos22:errikos2160@firstcluster.inftsar.mongodb.net/billyDatabase?retryWrites=true&w=majority";
+async function connect() {
+  try {
+    await mongoose.connect(uri).then(console.log("Connected on Mongo DB"));
+  } catch (error) {
+    console.error(error);
+  }
+}
+connect();
+
+const taskSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  checked: Boolean,
+});
+const Task = new mongoose.model("todotask", taskSchema);
+
+app.get("/info", (req, res) => {
+  res.status(200).json({ info: "preset text" });
+});
+
+app.get("/tasks", async (req, res) => {
+  let result;
+  try {
+    result = await Task.find();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+  res.send(result);
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  let result;
+  let tasks;
+  try {
+    tasks = await Task.find();
+    if (tasks.find((c) => c.id == req.params.id)) {
+      result = await Task.find({ id: req.params.id });
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("Task Not Found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/tasks", async (req, res) => {  
+  let newTask;
+  let tasks = await Task.find();
+  let taskId;
+  if (!req.body.name) {
+    res.status(400).send("Task Name is Required.");
+    return;
+  }
+  if (req.body.name.length > 100) {
+    res.status(400).send("Task Name must be less than 100 characters.");
+    return;
+  }
+
+  if (tasks.length == 0) {
+    taskId = 0;
+  } else {
+    taskId = tasks[tasks.length - 1].id;
+  }
+
+  try {
+    newTask = new Task({
+      id: taskId + 1,
+      name: req.body.name,
+      checked: false,
+    });
+    await newTask.save();
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send(newTask);
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  let tasks = await Task.find();
+  let task = tasks.find((c) => c.id == req.params.id);
+
+  try {
+    if (task) {
+      await task.set({
+        checked: req.body.checked,
+      });
+      await task.save();
+      res.status(200).send(task);
+    } else {
+      res.status(404).send("Task Not Found"); 
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  let tasks = await Task.find();
+  let task = tasks.find((c) => c.id == req.params.id);
+  try {
+    if (task) {
+      await Task.deleteOne({ id: req.params.id });
+      res.status(200).send(task);
+    } else {
+      res.status(404).send("Task Not Found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
--- front-end/index.js
+++ front-end/index.js
@@ -2,54 +2,232 @@
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
+let list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
     }
-}, false);
+  },
+  false
+);
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
+function newElement(input) {
+  let li = document.createElement("li");
+  let t = document.createTextNode(`${input.id}. ${input.name}`);
+  li.appendChild(t);
+  li.setAttribute("id", `li${input.id}`);
+  li.setAttribute("onclick", `updateTask(${input.id})`);
+  li.setAttribute("class", "");
+  document.getElementById("myUL").appendChild(li);
+  document.getElementById("myInput").value = "";
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.setAttribute("id", `span ${input.id}`);
+  span.setAttribute("onclick", `deleteTask(${input.id})`);
+  span.appendChild(txt);
+  li.appendChild(span);
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+function createTaskList(input) {
+  let list = document.getElementById("myUL");
+  list.innerHTML = "";
+  for (let i = 0; i < input.length; i++) {
+    let li = document.createElement("li");
+    let inputValue = input[i];
+    let inputName = input[i].name;
+    let inputId = input[i].id;
+    let t = document.createTextNode(`${inputId}. ${inputName}`);
     li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    li.setAttribute("id", `li${inputId}`);
+    li.setAttribute("onclick", `updateTask(${inputId})`);
+    document.getElementById("myUL").appendChild(li);
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
     span.className = "close";
+    span.setAttribute("id", `span ${inputId}`);
+    span.setAttribute("onclick", `deleteTask(${inputId})`);
     span.appendChild(txt);
     li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
+  }
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+    document.getElementById("myInput").value = "";
+  }
 }
+
+async function fetchTasks() {
+  try {
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const tasks = await response.json();
+    tasks.sort((a, b) => a.id - b.id);
+    if (response.status == 404) {
+      return "error";
+    }
+    createTaskList(tasks);
+    checkMarks(tasks);
+    console.log(tasks);
+    return tasks;
+
+    async function checkMarks(tasks) {
+      for (i = 0; i < tasks.length; i++) {
+        let checked = tasks[i].checked;
+        if (checked === true) {
+          let li = document.getElementById(`li${tasks[i].id}`);
+          if (li) li.setAttribute("class", "checked");
+        }
+        if (checked === false) {
+          let li = document.getElementById(`li${tasks[i].id}`);
+          if (li) li.setAttribute("class", "");
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function fetchTask() {
+  try {
+    let path = requestedPath();
+    const response = await fetch(path, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const task = await response.json();
+    clearText("getId", "getName");
+    return task;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function postTask() {
+  try {
+    let input = document.getElementById("myInput");
+    let inputName = document.getElementById("myInput").value;
+    let newTask = { name: inputName };
+
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    });
+    if (!response.ok) {
+      console.log(`Error status ${response.status}. ${response.statusText}`);
+      return;
+    } else {
+      const task = await response.json();
+      newElement(task);
+      console.log(task);
+      return task;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateTask(id) {
+  let path = `http://localhost:3000/tasks/${id}`;
+  let li = document.getElementById(`li${id}`);
+  let updatedTask;
+  if (li.className === "checked") {
+    updatedTask = { checked: false };
+  }
+  if (li.className === "") {
+    updatedTask = { checked: true };
+  }
+  try {
+    const response = await fetch(path, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+ 
+    if (!response.ok) {
+        console.log(`Error status ${response.status}. ${response.statusText}. Initializing PUT request in non-existant Task.`);
+        return;
+    } else {
+      const task = await response.json();
+
+      console.log(task);
+      return task;
+    }  
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteTask(ID) {
+  let path;
+  let deletedTask;
+  path = `http://localhost:3000/tasks/${ID}`;
+  deletedTask = { id: ID };
+  try {
+    const response = await fetch(path, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(deletedTask),
+    })
+    if (!response.ok) {
+        console.log(`Error status ${response.status}. ${response.statusText}.`);
+        return;
+    } else {
+    const task = await response.json();
+    console.log(task);
+    fetchTasks();
+    return;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//Auto-Add Task with Enter Key.
+let input = document.getElementById("myInput");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    document.getElementById("postBtn").click();
+  }
+});
+//Block special characters. Front - End Input Validation.
+function blockSpecialChar(event){
+    if(!((event.keyCode >= 65) && (event.keyCode <= 90) || (event.keyCode >= 97) && (event.keyCode <= 122) || (event.keyCode >= 48) && (event.keyCode <= 57))){
+       event.returnValue = false;
+       alert("No Special Characters Allowed");
+       return;
+    }
+    
+    event.returnValue = true;
+ }
